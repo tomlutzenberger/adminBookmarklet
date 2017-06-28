bb4f69e66a64aecce09ea6c8696a742ac53c360c
Tom Lutzenberger <lutzenbergerthomas@gmail.com>
2017-06-28 02:23:40 +0200

    [:truck: RENAME] Rename project to "adminBookmarklet"
    More clarity regarding what the project is about
    
    Fixes ---
    Related to ---

--- test/test.js
+++ test/test.js
@@ -11,33 +11,33 @@ global.window = mock.getWindow();
 global.document = window.document;
 
 
-const script = rewire('../adminBookmark.js');
-const adminBookmark = script.__get__('adminBookmark');
+const script = rewire('../adminBookmarklet.js');
+const adminBookmarklet = script.__get__('adminBookmarklet');
 
 
-describe('adminBookmark', () => {
+describe('adminBookmarklet', () => {
 
     describe('#getSystems()', () => {
         it('should return an array', () => {
-            assert.ok(Array.isArray(adminBookmark().getSystems()));
+            assert.ok(Array.isArray(adminBookmarklet().getSystems()));
         });
 
         it('should contain at least 1 element', () => {
-            assert.ok(adminBookmark().getSystems().length > 0);
+            assert.ok(adminBookmarklet().getSystems().length > 0);
         });
     });
 
 
     describe('#parseHead()', () => {
         it('should return a string', () => {
-            assert.ok(typeof adminBookmark().parseHead() === 'string');
+            assert.ok(typeof adminBookmarklet().parseHead() === 'string');
         });
     });
 
 
     describe('#getAdminPath()', () => {
         it('should return a string', () => {
-            assert.ok(typeof adminBookmark().getAdminPath() === 'string');
+            assert.ok(typeof adminBookmarklet().getAdminPath() === 'string');
         });
 
         it('should return "wp-admin" for wordpress sites', () => {
@@ -46,7 +46,7 @@ describe('adminBookmark', () => {
             meta.content = 'Wordpress';
             document.getElementsByTagName('head')[0].appendChild(meta);
 
-            assert.ok(adminBookmark().getAdminPath() === 'wp-admin');
+            assert.ok(adminBookmarklet().getAdminPath() === 'wp-admin');
         });
 
         it('should return "typo3" for Typo3 sites', () => {
@@ -55,7 +55,7 @@ describe('adminBookmark', () => {
             meta.content = 'TYPO3';
             document.getElementsByTagName('head')[0].appendChild(meta);
 
-            assert.ok(adminBookmark().getAdminPath() === 'typo3');
+            assert.ok(adminBookmarklet().getAdminPath() === 'typo3');
         });
     });
 
