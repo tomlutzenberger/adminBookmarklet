0dc30acb6c473e59ed700434d5e75fb9f242d841
Tom Lutzenberger <lutzenbergerthomas@gmail.com>
2017-06-28 02:14:35 +0200

    [:hammer: REFACTOR] Refactor to ES6
    To modernize and optimize the code, it was refactored to ES6
    
    Fixes ---
    Related to ---

--- adminBookmark.js
+++ adminBookmark.js
@@ -11,53 +11,95 @@
  */
 
 /*globals document,window*/
-
-(function () {
-
-  'use strict';
-
-  var systems = [
-    { name: 'cloudrexx',   path : 'cadmin'        },
-    { name: 'drupal',      path : 'user'          },
-    { name: 'jimdo',       path : 'login'         },
-    { name: 'joomla',      path : 'administrator' },
-    { name: 'magento',     path : 'admin'         },
-    { name: 'pimcore',     path : 'admin'         },
-    { name: 'sefrengo',    path : 'backend'       },
-    { name: 'typo3',       path : 'typo3'         },
-    { name: 'weebly',      path : 'login'         },
-    { name: 'wordpress',   path : 'wp-admin'      },
-    { name: 'xt-commerce', path : 'login.php'     }
-  ];
+/*jslint esnext:true */
 
 
+const adminBookmark = () => {
 
-  function parseHead() {
-    /* Select the content of <head>, strip all whitespaces and return */
-    var head = document.getElementsByTagName('head')[0].innerHTML;
+    'use strict';
 
-    return head.replace(/\s/g, '');
-  }
-
+    const ZERO = 0;
+    const FIRST = 0;
 
 
-  function getAdminPath() {
-    var
-      system = [],
-      headContent = parseHead();
+    const getSystems = () => {
+        return [
+            {
+                name: 'cloudrexx',
+                path: 'cadmin'
+            },
+            {
+                name: 'drupal',
+                path: 'user'
+            },
+            {
+                name: 'jimdo',
+                path: 'login'
+            },
+            {
+                name: 'joomla',
+                path: 'administrator'
+            },
+            {
+                name: 'magento',
+                path: 'admin'
+            },
+            {
+                name: 'pimcore',
+                path: 'admin'
+            },
+            {
+                name: 'sefrengo',
+                path: 'backend'
+            },
+            {
+                name: 'typo3',
+                path: 'typo3'
+            },
+            {
+                name: 'weebly',
+                path: 'login'
+            },
+            {
+                name: 'wordpress',
+                path: 'wp-admin'
+            },
+            {
+                name: 'xt-commerce',
+                path: 'login.php'
+            }
+        ];
+    };
 
-    system = systems.filter(function (systemObj) {
-      var pattern = new RegExp(systemObj.name, 'im');
-
-      return pattern.test(headContent);
-    });
-
-    return system.length > 0 ? system[0].path : 'admin';
-  }
 
 
+    const parseHead = () => {
+        /* Select the content of <head>, strip all whitespaces and return */
+        let head = document.getElementsByTagName('head')[FIRST].innerHTML;
 
-  // Open a new window/tab with domain + detected admin string
-  window.open(window.location.protocol + '//' + window.location.host + '/' + getAdminPath(), '_blank');
+        return head.replace(/\s/g, '');
+    };
 
-}());
+
+
+    const getAdminPath = () => {
+        let
+            system = [],
+            headContent = parseHead();
+
+        system = getSystems().filter((systemObj) => {
+            let pattern = new RegExp(systemObj.name, 'im');
+
+            return pattern.test(headContent);
+        });
+
+        return system.length > ZERO ? system[FIRST].path : 'admin';
+    };
+
+
+
+    return {getSystems, parseHead, getAdminPath};
+};
+
+
+window.open(window.location.protocol + '//' + window.location.host + '/' + adminBookmark().getAdminPath(), '_blank');
