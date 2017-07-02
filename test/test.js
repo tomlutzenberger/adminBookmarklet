231a393ed7bbba5cdf73222265f33e935a9614de
Tom Lutzenberger <lutzenbergerthomas@gmail.com>
2017-07-02 23:26:49 +0200

    [:white_check_mark: TESTS] Add tests for all systems
    Fixes ---
    Related to ---

--- test/test.js
+++ test/test.js
@@ -58,14 +58,59 @@ describe('adminBookmarklet', () => {
             assert.ok(typeof adminBookmarklet().getAdminPath() === 'string');
         });
 
-        it('should return "wp-admin" for Wordpress sites', () => {
-            addMetaGenerator('Wordpress');
-            assert.ok(adminBookmarklet().getAdminPath() === 'wp-admin');
+        it('should return "cadmin" for Cloudrexx sites', () => {
+            addMetaGenerator('Cloudrexx');
+            assert.ok(adminBookmarklet().getAdminPath() === 'cadmin');
+        });
+
+        it('should return "user" for Drupal sites', () => {
+            addMetaGenerator('Drupal');
+            assert.ok(adminBookmarklet().getAdminPath() === 'user');
+        });
+
+        it('should return "login" for Jimdo sites', () => {
+            addMetaGenerator('Jimdo');
+            assert.ok(adminBookmarklet().getAdminPath() === 'login');
+        });
+
+        it('should return "administrator" for Joomla sites', () => {
+            addMetaGenerator('Joomla');
+            assert.ok(adminBookmarklet().getAdminPath() === 'administrator');
+        });
+
+        it('should return "admin" for Magento sites', () => {
+            addMetaGenerator('Magento');
+            assert.ok(adminBookmarklet().getAdminPath() === 'admin');
+        });
+
+        it('should return "admin" for Pimcore sites', () => {
+            addMetaGenerator('Pimcore');
+            assert.ok(adminBookmarklet().getAdminPath() === 'admin');
+        });
+
+        it('should return "backend" for Sefrengo sites', () => {
+            addMetaGenerator('Sefrengo');
+            assert.ok(adminBookmarklet().getAdminPath() === 'backend');
         });
 
         it('should return "typo3" for Typo3 sites', () => {
             addMetaGenerator('TYPO3');
             assert.ok(adminBookmarklet().getAdminPath() === 'typo3');
+        });
+
+        it('should return "wp-admin" for Wordpress sites', () => {
+            addMetaGenerator('Wordpress');
+            assert.ok(adminBookmarklet().getAdminPath() === 'wp-admin');
+        });
+
+        it('should return "login" for Weebly sites', () => {
+            addMetaGenerator('Weebly');
+            assert.ok(adminBookmarklet().getAdminPath() === 'login');
+        });
+
+        it('should return "login.php" for xt-commerce sites', () => {
+            addMetaGenerator('xt-commerce');
+            assert.ok(adminBookmarklet().getAdminPath() === 'login.php');
         });
     });
 
